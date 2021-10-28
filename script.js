bc3d5c626d399967eced44d4d3683c32ddeb4568
Michael Paulsen <michael.j.paulsen@gmail.com>
2021-10-28 16:07:39 +0000

    made script.js use regular javasctipt also made it so you can regenerate the stats. also wrapped the stat  maker into a function so that you don't have to reload to re-roll

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 function maxRandom(max){ 
+ /**
+  * returns a number from [1-max] 
+  */
   return Math.floor(Math.random()*max)+1;
 }
 function makeStat(rolls){ 
+  /**
+   * rolls a die rolls time and discards the worst 
+   */
   let temarr = [];
   let total = 0;  
+  let minNotChosen = true; 
   for(let x = 0; x < rolls; x++){
     temarr.push(maxRandom(6))
   }
   for(let y = 0; y < rolls; y++){ 
-    if(temarr[y] != Math.min(...temarr)){ 
-      total += temarr[y];
+    if(temarr[y] == Math.min(...temarr) && minNotChosen){ /**
+    *if the curent index is the lowest (or tied for the lowest) in the array then if we haven't already discarded a number discard this number.  
+    *minNotChosen fixes a bug where if all of the numbers are the same it is zero because it never adds them
+    */  
+      minNotChosen = false; 
+      continue;       
     }
+    total += temarr[y];
+    // get the total to return it
+
   }
   return total;
 }
-//console.log($); 
-for(let x = 0; x < 6; x++){ 
-  //console.log(" roll :", x,", " ,); 
-  $("#output").append("<p> stat " + (x+1) + " : " + makeStat(3) + "</p>"); 
+function rollStats(){ 
+  let el = document.getElementById("output"); 
+  el.innerHTML = "";
+  for(let x = 0; x < 6; x++){ 
+    el.innerHTML += ("<p> stat " + (x+1) + " : " + makeStat (3) + "</p>") 
+  }
 }
+window.addEventListener('DOMContentLoaded', (event) => {
+  rollStats(); 
+});
